a04354467aa70b68e1ec6251b9841a7a0ce8db78
Juraj Kmotorka <jurajkmo@gmail.com>
2023-03-20 23:08:34 +0100

    fixing, attempt 2

--- src/server/api/trpc/dates/index.ts
+++ src/server/api/trpc/dates/index.ts
@@ -1,19 +1,20 @@
-import { z } from "zod";
 import { dateSchema } from "./model";
 
 import { createTRPCRouter, publicProcedure } from "(/)/server/api/trpc";
 
 export const datesRouter = createTRPCRouter({
-  reserveDate: publicProcedure.input(dateSchema).mutation(async (input) => {
-    const url = "https://volejbal.c1-europe.altogic.com/api/dates";
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ date: input }),
-    });
-    const data = await response.json();
-    return data;
-  }),
+  reserveDate: publicProcedure
+    .input(dateSchema)
+    .mutation(async (input): Promise<Record<string, unknown>> => {
+      const url = "https://volejbal.c1-europe.altogic.com/api/dates";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ date: input }),
+      });
+      const data: Record<string, unknown> = await response.json();
+      return data;
+    }),
 });
